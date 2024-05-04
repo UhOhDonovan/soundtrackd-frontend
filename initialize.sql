b9ed379a5ff4b29807c0b0224589c8436f8d29c9
Adam Burton <adam_burton1@outlook.com>
2024-05-04 03:05:05 -0500

    update initialize.sql capitalization to match online database

--- initialize.sql
+++ initialize.sql
@@ -3,35 +3,35 @@ DROP DATABASE IF EXISTS soundtrackd;
 CREATE DATABASE soundtrackd;
 USE soundtrackd;
 
-CREATE TABLE USER(
+CREATE TABLE user(
     email VARCHAR(254) UNIQUE NOT NULL,
     username VARCHAR(60) PRIMARY KEY,
     password VARCHAR(64) NOT NULL
 );
 
-CREATE TABLE FOLLOWS_USER(
+CREATE TABLE follows_user(
     follower VARCHAR(60),
     followed VARCHAR(60),
     PRIMARY KEY (follower, followed),
-    CONSTRAINT fk_follower_email FOREIGN KEY (follower) REFERENCES USER(username),
-    CONSTRAINT fk_followed_email FOREIGN KEY (followed) REFERENCES USER(username)
+    CONSTRAINT fk_follower_email FOREIGN KEY (follower) REFERENCES user(username),
+    CONSTRAINT fk_followed_email FOREIGN KEY (followed) REFERENCES user(username)
 );
 
-CREATE TABLE ARTIST(
+CREATE TABLE artist(
     id VARCHAR(22) PRIMARY KEY,
     name VARCHAR(254) NOT NULL,
     spotify_link VARCHAR(254) NOT NULL
 );
 
-CREATE TABLE FOLLOWS_ARTIST(
+CREATE TABLE follows_artist(
     user_id VARCHAR(60),
     artist_id VARCHAR(22),
     PRIMARY KEY (user_id, artist_id),
-    CONSTRAINT follows_fk_user_id FOREIGN KEY (user_id) REFERENCES USER(username),
-    CONSTRAINT follows_fk_artist_id FOREIGN KEY (artist_id) REFERENCES ARTIST(id)
+    CONSTRAINT follows_fk_user_id FOREIGN KEY (user_id) REFERENCES user(username),
+    CONSTRAINT follows_fk_artist_id FOREIGN KEY (artist_id) REFERENCES artist(id)
 );
 
-CREATE TABLE ALBUM(
+CREATE TABLE album(
     id VARCHAR(22) PRIMARY KEY,
     title VARCHAR(254) NOT NULL,
     release_date DATE NOT NULL,
@@ -40,15 +40,15 @@ CREATE TABLE ALBUM(
     image_link VARCHAR(254) NOT NULL
 );
 
-CREATE TABLE RELEASED_ALBUM(
+CREATE TABLE released_album(
     artist_id VARCHAR(22) NOT NULL,
     album_id VARCHAR(22) NOT NULL,
     PRIMARY KEY (artist_id, album_id),
-    CONSTRAINT released_album_fk_artist_id FOREIGN KEY (artist_id) REFERENCES ARTIST(id),
-    CONSTRAINT released_album_fk_album_id FOREIGN KEY (album_id) REFERENCES ALBUM(id)
+    CONSTRAINT released_album_fk_artist_id FOREIGN KEY (artist_id) REFERENCES artist(id),
+    CONSTRAINT released_album_fk_album_id FOREIGN KEY (album_id) REFERENCES album(id)
 );
 
-CREATE TABLE REVIEW(
+CREATE TABLE review(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     posted_by VARCHAR(60) NOT NULL,
     album_id VARCHAR(22) NOT NULL,
@@ -56,51 +56,51 @@ CREATE TABLE REVIEW(
     post_time TIME NOT NULL,
     rating INTEGER,
     body TEXT,
-    CONSTRAINT review_fk_posted_by FOREIGN KEY (posted_by) REFERENCES USER(username),
-    CONSTRAINT review_fk_album_id FOREIGN KEY (album_id) REFERENCES ALBUM(id)
+    CONSTRAINT review_fk_posted_by FOREIGN KEY (posted_by) REFERENCES user(username),
+    CONSTRAINT review_fk_album_id FOREIGN KEY (album_id) REFERENCES album(id)
 );
 
-CREATE TABLE REVIEW_COMMENT(
+CREATE TABLE review_comment(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     review_id INTEGER NOT NULL,
     posted_by VARCHAR(60) NOT NULL,
     post_date DATE NOT NULL,
     post_time TIME NOT NULL,
     body TEXT,
-    CONSTRAINT review_comment_fk_review_id FOREIGN KEY (review_id) REFERENCES REVIEW(id),
-    CONSTRAINT review_comment_fk_posted_by FOREIGN KEY (posted_by) REFERENCES USER(username)
+    CONSTRAINT review_comment_fk_review_id FOREIGN KEY (review_id) REFERENCES review(id),
+    CONSTRAINT review_comment_fk_posted_by FOREIGN KEY (posted_by) REFERENCES user(username)
 );
 
-CREATE TABLE TRACK(
+CREATE TABLE track(
     id VARCHAR(22) PRIMARY KEY,
     title VARCHAR(254) NOT NULL,
     spotify_link VARCHAR(254) NOT NULL
 );
 
-CREATE TABLE RELEASED_TRACK(
+CREATE TABLE released_track(
     artist_id VARCHAR(22),
     track_id VARCHAR(22),
     PRIMARY KEY (artist_id, track_id),
-    CONSTRAINT released_track_fk_artist_id FOREIGN KEY (artist_id) REFERENCES ARTIST(id),
-    CONSTRAINT released_track_fk_track_id FOREIGN KEY (track_id) REFERENCES TRACK(id)
+    CONSTRAINT released_track_fk_artist_id FOREIGN KEY (artist_id) REFERENCES artist(id),
+    CONSTRAINT released_track_fk_track_id FOREIGN KEY (track_id) REFERENCES track(id)
 );
 
-CREATE TABLE TRACK_COMMENT(
+CREATE TABLE track_comment(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     track_id VARCHAR(22) NOT NULL,
     posted_by VARCHAR(60) NOT NULL,
     post_date DATE NOT NULL,
     post_time TIME NOT NULL,
     body TEXT,
-    CONSTRAINT track_comment_fk_track_id FOREIGN KEY (track_id) REFERENCES TRACK(id),
-    CONSTRAINT track_comment_fk_posted_by FOREIGN KEY (posted_by) REFERENCES USER(username)
+    CONSTRAINT track_comment_fk_track_id FOREIGN KEY (track_id) REFERENCES track(id),
+    CONSTRAINT track_comment_fk_posted_by FOREIGN KEY (posted_by) REFERENCES user(username)
 );
 
-CREATE TABLE APPEARS_ON(
+CREATE TABLE appears_on(
     track_id VARCHAR(22),
     album_id VARCHAR(22),
     track_num INTEGER NOT NULL,
     PRIMARY KEY (track_id, album_id),
-    CONSTRAINT appears_on_fk_album_id FOREIGN KEY (album_id) REFERENCES ALBUM(id),
-    CONSTRAINT appears_on_fk_track_id FOREIGN KEY (track_id) REFERENCES TRACK(id)
+    CONSTRAINT appears_on_fk_album_id FOREIGN KEY (album_id) REFERENCES album(id),
+    CONSTRAINT appears_on_fk_track_id FOREIGN KEY (track_id) REFERENCES track(id)
 );
