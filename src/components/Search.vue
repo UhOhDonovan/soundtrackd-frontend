5d38763f22a394a17f90e0da0240a159dda45513
Adam Burton <adam_burton1@outlook.com>
2024-11-05 03:23:53 -0600

    Got search to retrieve and display results

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,23 +1,52 @@
-<script setup lang="ts">
-import { computed } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
-
-const router = useRouter()
-const route = useRoute()
-
-const search = computed({
-  get() {
-    return route.query.search ?? ''
+<script lang="ts">
+// import { computed } from 'vue'
+// import { useRoute, useRouter } from 'vue-router'
+export default {
+  data() {
+    return {
+      search_results: [{
+        name: '',
+        artists: [{name: ''}],
+        images: [{url: ''}]
+      }],
+      url_root: "http://localhost:5345/search/album",
+      q: ""
+    }
   },
-  set(search) {
-    router.replace({ query: { search } })
+  methods: {
+    search_spotify() {
+      fetch(this.url_root + "?q=" + this.q)
+      .then(res => res.json())
+      .then(data => this.search_results = data['items'])
+    }
   }
-})
+  
+}
+
+
+// const router = useRouter()
+// const route = useRoute()
+// const url_root = "http://localhost:5345/search/album";
+// let q = "";
+// let result = 1;
+
+// const search_spotify = () => {
+//     result = result + 1;
+//     fetch(url_root + "?q=" + q)
+//     .then(res => res.json())
+//     .then(data => console.log(data))
+//   }
 </script>
 
 <template>
     <h2>Search</h2>
-    <label>
-        Search: <input v-model.trim="search" maxlength="20">
-    </label>
+    <input v-model.trim="q" maxlength="20">
+    <button @click="search_spotify()" class="col-2 button">
+      Search
+    </button>
+    <div v-for="result in search_results"> 
+      <p>{{ result['name'] }}</p>
+      <img :src="`${result['images'][0]['url']}`" style="width:100px"></img>
+      <p v-for="artist in result['artists']">{{ artist['name'] }}</p>
+    </div>
 </template>
