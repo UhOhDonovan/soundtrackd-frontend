4f7de1cab4429aa8965c9a6f5892d6a66f12fbc9
Donovan Bale <donovan@balehaus.org>
2024-12-02 18:45:44 -0600

    Login JWT storage

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,50 +1,54 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router';
-const router = useRouter()
+import { useRouter } from "vue-router";
+const router = useRouter();
 
-let email = ""
-let username = ""
-let password = ""
-let error = ""
+let email = "";
+let username = "";
+let password = "";
+let password_confirmation = "";
+let error = "";
 
 function register() {
   if (password.length < 3) {
     console.log("password too short");
-  }
-  else {
-    sendInfo(email, username, password)
+  } else {
+    sendInfo(email, username, password, password_confirmation);
     //router.push({ path: '/login'})
   }
 }
 
-const sendInfo = async (email: string, username: string, password: string) => {
+const sendInfo = async (
+  email: string,
+  username: string,
+  password: string,
+  password_confirmation: string
+) => {
   // IK this is sketchy but it's the only way I could get it to work
   const request = {
     email: email,
     username: username,
     password: password,
-  }
+    password_confirmation: password_confirmation,
+  };
 
-  const response = await fetch(`http://localhost:5345/register`, {
+  const response = await fetch(`http://localhost:5345/users/register`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(request)
-  })
+    body: JSON.stringify(request),
+  });
 
-  console.log(response)
+  console.log(response);
   if (!response.ok) {
-    error = response.statusText
-    console.log(`Error: ${error}`)
-  }
-  else {
-    router.push({ path: '/login' })
+    error = response.statusText;
+    console.log(`Error: ${error}`);
+  } else {
+    router.push({ path: "/login" });
   }
 
-  console.log(response.ok)
-}
-
+  console.log(response.ok);
+};
 </script>
 
 <template>
@@ -54,28 +58,35 @@ const sendInfo = async (email: string, username: string, password: string) => {
     <form @submit.prevent="register">
       <div class="form-group">
         <label for="email">Email: </label>
-        <input type="email" name="email" v-model="email">
+        <input type="email" name="email" v-model="email" />
       </div>
       <div class="form-group">
         <label for="username">Username: </label>
-        <input type="username" name="username" v-model="username">
+        <input type="username" name="username" v-model="username" />
       </div>
       <div class="form-group">
         <label for="password">Password: </label>
-        <input type="password" name="password" v-model="password">
+        <input type="password" name="password" v-model="password" />
       </div>
-      <br>
+      <div class="form-group">
+        <label for="password_confirmation">Confirm Password: </label>
+        <input
+          type="password"
+          name="password_confirmation"
+          v-model="password_confirmation"
+        />
+      </div>
+      <br />
       <button type="submit" class="button">Register</button>
     </form>
 
-    <p>Already have an account? <RouterLink to="/Login">Log in!</RouterLink>
-    </p>
+    <p>Already have an account? <RouterLink to="/Login">Log in!</RouterLink></p>
   </div>
 </template>
 
 <style scoped>
 .button {
-  background-color: #00D3C0;
+  background-color: #00d3c0;
 }
 
 .button:hover {
