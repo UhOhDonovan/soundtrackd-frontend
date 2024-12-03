937a0d11e60d8c271c7ec2d20b300541d9979d0a
Donovan Bale <donovan@balehaus.org>
2024-12-03 13:51:37 -0600

    fix hello world conflict

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,63 +1,50 @@
 <script lang="ts">
 // import { ref } from 'vue'
-<<<<<<< HEAD
 export default {
   props: {
     msg: String,
   },
   data() {
-    return{
+    return {
       test: localStorage.getItem("token"),
       username: localStorage.getItem("username"),
-    }
+    };
   },
   methods: {
     testAPI: async () => {
-      const response = await fetch("http://localhost:5345/", {
-        method: "GET"
-      })
-      console.log(await response.json())
-    }
+      const response = await fetch("/api/", {
+        method: "GET",
+      });
+      console.log(await response.json());
+    },
   },
-  mounted(){
-    window.addEventListener('user-changed', (event) => {
-      console.log("hworld event recieved")
+  mounted() {
+    window.addEventListener("user-changed", (event) => {
+      console.log("hworld event recieved");
       this.username = (<any>event).detail.storage;
     });
-  }
-=======
-let test = localStorage.getItem("token")
-
-defineProps<{ msg: string }>()
-
-const testAPI = async () => {
-  const response = await fetch("/api", {
-    method: "GET"
-  })
-  console.log(await response.json())
->>>>>>> main
-}
+  },
+};
 </script>
 
 <template>
   <div class="card">
     <h1 class="row">{{ msg }}</h1>
     <div class="row justify-content-center" v-if="username == ''">
-      <RouterLink to="/login" class="col-2 button">
-        Login
-      </RouterLink>
-      <br>
-      <RouterLink to="/register" class="col-2 button">
-        Register
-      </RouterLink>
+      <RouterLink to="/login" class="col-2 button"> Login </RouterLink>
+      <br />
+      <RouterLink to="/register" class="col-2 button"> Register </RouterLink>
       <button hidden="true" @click="testAPI()" class="col-2 button">
         tester
       </button>
     </div>
     <div v-else>
-      <h2 style="font-weight:400"><RouterLink to="/search">Search for an album</RouterLink> to get started reading and writing reviews!</h2>
+      <h2 style="font-weight: 400">
+        <RouterLink to="/search">Search for an album</RouterLink> to get started
+        reading and writing reviews!
+      </h2>
     </div>
-    <p hidden="true">{{test }}</p>
+    <p hidden="true">{{ test }}</p>
   </div>
 </template>
 
